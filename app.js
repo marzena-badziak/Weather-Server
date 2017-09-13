6ecb2a98cc31dbe02bcfc20132aaf3cbc2404d0d
marzena-badziak <marzena.badziak@gmail.com>
2017-09-13 23:26:47 +0200

    improved logic in email and city validation

--- app.js
+++ app.js
@@ -15,41 +15,52 @@ app.get("/weather", function(req, res) {
     res.send(JSON.stringify(resp));
   };
 
-  if (!reg.test(email)) {
-    createResp({ success: false, response: "invalid email", data: "" });
-    console.log("invalid email!");
+  if (
+    reg.test(email) &&
+    (city.toLowerCase() === "london" || city.toLowerCase() === "new york")
+  ) {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&APPID=ad39bc99ad9b6537af039f557c68561f"
+      )
+      .then(response => {
+        createResp({
+          success: true,
+          response: `hello ${email}, the weather in ${city} is: ${response.data
+            .weather[0].main}`,
+          data: {
+            description: response.data.weather[0].description,
+            temperature: response.data.main.temp,
+            pressure: response.data.main.pressure,
+            wind: response.data.wind,
+            humidity: response.data.main.humidity,
+            visibility: response.data.visibility
+          }
+        });
+      })
+      .catch(err => {
+        createResp({ success: false, response: err, data: "" });
+        console.log(err);
+      });
   } else {
     if (
+      !reg.test(email) &&
       !(city.toLowerCase() === "london" || city.toLowerCase() === "new york")
     ) {
+      createResp({
+        success: false,
+        response: "invalid email and city",
+        data: ""
+      });
+      console.log("invalid email and city!");
+    } else if (!reg.test(email)) {
+      createResp({ success: false, response: "invalid email", data: "" });
+      console.log("invalid email!");
+    } else {
       createResp({ success: false, response: "invalid city", data: "" });
       console.log("You can check weather only in London or New York!");
-    } else {
-      axios
-        .get(
-          "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&APPID=ad39bc99ad9b6537af039f557c68561f"
-        )
-        .then(response => {
-          createResp({
-            success: true,
-            response: `hello ${email}, the weather in ${city} is: ${response
-              .data.weather[0].main}`,
-            data: {
-              description: response.data.weather[0].description,
-              temperature: response.data.main.temp,
-              pressure: response.data.main.pressure,
-              wind: response.data.wind,
-              humidity: response.data.main.humidity,
-              visibility: response.data.visibility
-            }
-          });
-        })
-        .catch(err => {
-          createResp({ success: false, response: err, data: "" });
-          console.log(err);
-        });
     }
   }
 });
